0b7c4e71cee33193c469188d8b2c45b247f92692
mhrpwfc88 <mh17pwfc@gmail.com>
2024-08-21 19:49:23 +0700

    tambah bg sama btton

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["index.html"],
   theme: {
     extend: {
+      colors: {
+        primary : 'rgb(31 41 55);'
+      },
       fontFamily: {
         primary: 'courier-prime-bold',
         merri: 'merriweather-sans',
